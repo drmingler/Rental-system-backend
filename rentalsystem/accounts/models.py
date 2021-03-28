1bf62da690c6ad01ad4df7994bad1c981246bd4d
“Drmingler” <kayymedia@gmail.com>
2021-03-28 16:07:51 +0200

    feat: Add property Model

--- rentalsystem/accounts/models.py
+++ rentalsystem/accounts/models.py
@@ -10,19 +10,19 @@ class User(AbstractUser, AbstractBaseModel):
 
     FEMALE = "Female"
     MALE = "Male"
-    GenderChoice = [(MALE, "Male"), (FEMALE, "Female")]
+    GENDER_CHOICE = [(MALE, "Male"), (FEMALE, "Female")]
 
     LANDLORD = "Landlord"
     TENANT = "Tenant"
-    UserType = [(LANDLORD, "Landlord"), (TENANT, "Tenant")]
+    USER_TYPE = [(LANDLORD, "Landlord"), (TENANT, "Tenant")]
 
     email = EmailField(max_length=150, unique=True)
     username = CharField("Username", max_length=255, unique=True)
     phoneNumber = CharField(max_length=20, blank=True)
     birthDate = DateTimeField(max_length=128, blank=True, null=True)
     avatar = ImageField(blank=True, storage=MediaRootS3Boto3Storage())
-    gender = CharField(choices=GenderChoice, max_length=30, blank=True)
-    userType = CharField(choices=UserType, max_length=30, default=TENANT)
+    gender = CharField(choices=GENDER_CHOICE, max_length=30, blank=True)
+    userType = CharField(choices=USER_TYPE, max_length=30, default=TENANT)
     address = CharField(blank=True, max_length=255)
     nationality = CharField(blank=True, max_length=40)
 
--- rentalsystem/properties/models.py
+++ rentalsystem/properties/models.py
@@ -1,5 +1,44 @@
-from django.db import models
-
 # Create your models here.
+from django.db.models import (
+    ForeignKey,
+    CASCADE,
+    CharField,
+    BooleanField,
+    FloatField,
+    TextField,
+    DateField,
+)
+from django.forms import IntegerField
 
-# class Property(models.)
+from rentalsystem.accounts.models import User
+from rentalsystem.common.models import AbstractBaseModel
+
+
+class Property(AbstractBaseModel):
+    """ Property Model """
+
+    APARTMENT = "apartment"
+    HOUSE = "house"
+    CONDO = "condo"
+    DUPLEX = "duplex"
+    ROOM = "room"
+
+    PROPERTY_TYPE = (
+        (APARTMENT, "apartment"),
+        (HOUSE, "house"),
+        (CONDO, "condo"),
+        (DUPLEX, "duplex"),
+        (ROOM, "room"),
+    )
+    user = ForeignKey(User, related_name="properties", on_delete=CASCADE)
+    propertyName = CharField(max_length=250, blank=True)
+    propertyType = CharField(max_length=20, choices=PROPERTY_TYPE, blank=True)
+    numberOfBedrooms = IntegerField(max_value=20)
+    numberOfBathrooms = IntegerField(max_value=20)
+    unit = IntegerField(max_value=20)
+    size = FloatField(default=0.00)
+    listingDescription = TextField(blank=True)
+    availableFrom = DateField(max_length=20, blank=True)
+    monthlyRent = FloatField(default=0.00)
+    securityDeposit = FloatField(default=0.00)
+    isOwnerShipVerified = BooleanField(default=False, max_length=20)

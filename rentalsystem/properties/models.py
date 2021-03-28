ad17916cccbb23aadbd79e56ca40d7228ce8fb56
“Drmingler” <kayymedia@gmail.com>
2021-03-28 16:50:09 +0200

    feat: Add propertyAddress Model

--- rentalsystem/properties/models.py
+++ rentalsystem/properties/models.py
@@ -4,9 +4,10 @@ from django.db.models import (
     CASCADE,
     CharField,
     BooleanField,
-    FloatField,
     TextField,
     DateField,
+    DecimalField,
+    OneToOneField,
 )
 from django.forms import IntegerField
 
@@ -36,9 +37,30 @@ class Property(AbstractBaseModel):
     numberOfBedrooms = IntegerField(max_value=20)
     numberOfBathrooms = IntegerField(max_value=20)
     unit = IntegerField(max_value=20)
-    size = FloatField(default=0.00)
+    size = DecimalField(default=0.00)
     listingDescription = TextField(blank=True)
     availableFrom = DateField(max_length=20, blank=True)
-    monthlyRent = FloatField(default=0.00)
-    securityDeposit = FloatField(default=0.00)
+    monthlyRent = DecimalField(default=0.00)
+    securityDeposit = DecimalField(default=0.00)
     isOwnerShipVerified = BooleanField(default=False, max_length=20)
+
+
+class PropertyAddress(AbstractBaseModel):
+    """ Property's Address Model"""
+
+    property = OneToOneField(
+        Property, related_name="propertyAddress", on_delete=CASCADE
+    )
+    address = CharField(default=None, max_length=255)
+    stateName = CharField(max_length=50, blank=True)
+    latitude = DecimalField(default=None, blank=True)
+    longitude = DecimalField(default=None, blank=True)
+
+
+class PropertyRules(AbstractBaseModel):
+    """ Property's Rules Model"""
+
+    property = OneToOneField(Property, related_name="propertyRule", on_delete=CASCADE)
+    smoking = BooleanField(default=False)
+    pet = BooleanField(default=False)
+    musicalInstruments = BooleanField(default=False)

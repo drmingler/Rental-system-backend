1c7a8eb48a4dcee9c58cb13b314076a446d4521a
“Drmingler” <kayymedia@gmail.com>
2021-03-29 18:52:10 +0200

    feat: Rename accounts views

--- rentalsystem/accounts/urls.py
+++ rentalsystem/accounts/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import SimpleRouter
-from rentalsystem.accounts.views import AllUserProfileViewSet, LandlordPropertiesViewSet
+from rentalsystem.accounts.views import ProfileViewSet, LandlordProfileViewSet
 
 router = SimpleRouter()
-router.register("accounts", AllUserProfileViewSet)
-router.register("landlord", LandlordPropertiesViewSet)
+router.register("accounts", ProfileViewSet)
+router.register("landlord", LandlordProfileViewSet)
 
 urlpatterns = router.urls
--- rentalsystem/accounts/views.py
+++ rentalsystem/accounts/views.py
@@ -1,9 +1,8 @@
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
-from rest_framework.response import Response
+from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
 from rest_framework.viewsets import GenericViewSet
 from .models import User
 from .serializers import UserSerializer
-from ..common.permission import IsOwner
+from ..common.permission import IsOwnProfile
 
 
 ## get user add permission to allow only user see only his own profile
@@ -12,13 +11,15 @@ from ..common.permission import IsOwner
 ## get properties by landlord id
 
 
-class AllUserProfileViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
-    permission_classes = [IsOwner]
+class ProfileViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    """Returns a user's profile either a landlord or tenant"""
+
+    permission_classes = [IsOwnProfile]
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
-class LandlordPropertiesViewSet(RetrieveModelMixin, GenericViewSet):
+class LandlordProfileViewSet(RetrieveModelMixin, GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 

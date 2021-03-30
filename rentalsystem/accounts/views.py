ad53aa0aaf6821c39938bd16071c6735da538ab8
“Drmingler” <kayymedia@gmail.com>
2021-03-30 06:07:42 +0200

    fix: Import in account view

--- rentalsystem/accounts/views.py
+++ rentalsystem/accounts/views.py
@@ -1,14 +1,9 @@
 from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
 from rest_framework.viewsets import GenericViewSet
-from .models import User
-from .serializers import UserSerializer
-from ..common.permission import IsOwnProfile
 
-
-## get user add permission to allow only user see only his own profile
-## update user
-## get landlord profile
-## get properties by landlord id
+from rentalsystem.accounts.models import User
+from rentalsystem.accounts.serializers import UserSerializer
+from rentalsystem.common.permission import IsOwnProfile
 
 
 class ProfileViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):

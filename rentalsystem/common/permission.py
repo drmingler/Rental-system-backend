7c6c595dc404015b4c683871f13219c6c291d512
“Drmingler” <kayymedia@gmail.com>
2021-03-30 06:32:30 +0200

    feat:Allow only landlords to edit and update properties

--- rentalsystem/common/permission.py
+++ rentalsystem/common/permission.py
@@ -1,27 +1,35 @@
 from rest_framework.permissions import BasePermission
 
+from rentalsystem.accounts.models import User
 
-class IsOwner(BasePermission):
+
+class AbstractBasePermission(BasePermission):
+    def has_permission(self, request, view) -> bool:
+        return request.user and request.user.is_authenticated
+
+
+class IsOwner(AbstractBasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
 
-    def has_permission(self, request, view) -> bool:
-        return request.user and request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj) -> bool:
-        print(obj)
         return request.user == obj.user
 
 
-class IsOwnProfile(BasePermission):
+class IsOwnProfile(AbstractBasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
 
-    def has_permission(self, request, view) -> bool:
-        return request.user and request.user.is_authenticated
+    def has_object_permission(self, request, view, obj) -> bool:
+        return request.user.id == obj.id
+
+
+class IsLandlord(AbstractBasePermission):
+    """
+    Custom permission to only allow landlords to add and edit properties.
+    """
 
     def has_object_permission(self, request, view, obj) -> bool:
-        print(obj)
-        return request.user.id == obj.id
+        return request.user.userType == User.LANDLORD

249cbc5ffed842f06eb2b113ac91e553e138a3a7
“Drmingler” <kayymedia@gmail.com>
2021-04-07 16:00:33 +0200

    feat: Add LastMessagesViewSet

--- rentalsystem/chat/urls.py
+++ rentalsystem/chat/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import SimpleRouter
 
-from rentalsystem.chat.views import ChatViewSet
+from rentalsystem.chat.views import ConversationViewSet, LastMessagesViewSet
 
 router = SimpleRouter()
-router.register("chat", ChatViewSet, basename="chat")
+router.register("conversation", ConversationViewSet, basename="chat")
+router.register("last-message", LastMessagesViewSet, basename="last_message")
 
 urlpatterns = router.urls
--- rentalsystem/chat/views.py
+++ rentalsystem/chat/views.py
@@ -1,18 +1,30 @@
-from django.db.models.query_utils import Q
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
-from rentalsystem.chat.models import Chat
 from rentalsystem.chat.serializers import ChatSerializer
+from rentalsystem.chat.service import ChatService
 
 
-class ChatViewSet(ListModelMixin, GenericViewSet):
+class ConversationViewSet(ListModelMixin, GenericViewSet):
+    """ This view  returns all the  messages from various conversations"""
+
+    chat_service = ChatService()
     serializer_class = ChatSerializer
 
     def get_queryset(self):
         user_id: str = self.request.query_params.get("user_id")
         user = self.request.user
-        return Chat.objects.filter(
-            Q(sender=user) & Q(receiver_id=user_id)
-            | Q(sender_id=user_id) & Q(receiver=user)
-        )
+        result = self.chat_service.get_conversation(user=user, user_id=user_id)
+        return result
+
+
+class LastMessagesViewSet(ListModelMixin, GenericViewSet):
+    """ This view returns the last message for each conversation"""
+
+    chat_service = ChatService()
+    serializer_class = ChatSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        result = self.chat_service.get_last_message(user=user)
+        return result
